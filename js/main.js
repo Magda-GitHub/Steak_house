05d6cabddcf4639e6ee6eac79e5e42c7702d2a3b
Magda-GitHub <antoniakmagdalena@wp.pl>
2022-07-19 22:36:42 +0200

    steak_house

---
+++ js/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentloaded', function(){
+
+const nav = document.querySelector('.navbar')
+const allNavItems = document.querySelectorAll('.nav-link')
+const navList = document.querySelector('.navbar-collapse')
+
+
+    function 
+
+
+/*allNavItems.forEach(item => item.addEventListener('click', () => navList?.classList.remove('show')))
+
+
+
+
+
+
+
+window.addEventListener('scroll, addShadow')
+
+
+})
